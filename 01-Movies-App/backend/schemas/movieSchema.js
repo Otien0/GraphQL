5ed695adaa00a7aa696c59b12655415c1600aed1
mourice-oduor <otienomaurice364@gmail.com>
2022-07-31 07:13:36 +0300

    update schema

--- 01-Movies-App/backend/schemas/movieSchema.js
+++ 01-Movies-App/backend/schemas/movieSchema.js
@@ -1,6 +1,15 @@
 const graphql = require("graphql");
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const movies = [
+    { title: 'Avatar', genre: 'Sci-fi', id: '1'},
+    { title: 'Titanic', genre: 'Romance', id: '2'},
+    { title: 'Lord Of The Rings', genre: 'Adventure', id: '3'},
+    { title: 'Pirates Of The Carribean', genre: 'Adventure', id: '4'},
+    { title: 'Avengers', genre: 'Sci-fi', id: '5'},
+]
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
@@ -19,6 +28,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString } },
             resolve(parrent, args) {
                 // Get data from database
+                return _.find(movies, { id: args.id })
             }
         }
     })
