422c21a322b6df36ed0ffab8b06f7b4a5188ab17
mourice-oduor <otienomaurice364@gmail.com>
2022-08-01 14:43:52 +0300

    GraphQL List Type

--- 01-Movies-App/backend/schemas/movieSchema.js
+++ 01-Movies-App/backend/schemas/movieSchema.js
@@ -1,19 +1,23 @@
 const graphql = require("graphql");
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
 const movies = [
     { title: 'Avatar', genre: 'Sci-fi', id: '1', directorId: '1'},
-    { title: 'Titanic', genre: 'Romance', id: '2', directorId: '2'},
-    { title: 'Lord Of The Rings', genre: 'Adventure', id: '3', directorId: '3'},
-    { title: 'Pirates Of The Carribean', genre: 'Adventure', id: '4', directorId: '4'},
-    { title: 'Avengers', genre: 'Sci-fi', id: '5', directorId: '5'}
+    { title: 'Titanic', genre: 'Romance', id: '2', directorId: '1'},
+    { title: 'The Killer', genre: 'Action', id: '3', directorId: '2'},
+    { title: 'Lord Of The Rings', genre: 'Adventure', id: '4', directorId: '3'},
+    { title: 'Pirates Of The Carribean', genre: 'Adventure', id: '5', directorId: '4'},
+    { title: 'Avengers', genre: 'Sci-fi', id: '6', directorId: '5'},
+    { title: 'Hard Boiled', genre: 'Action', id: '7', directorId: '2'},
+    { title: 'Face/Off', genre: 'Action', id: '8', directorId: '2'},
+    { title: 'Gray Man', genre: 'Action', id: '9', directorId: '5'}
 ]
 
 const directors = [
     { name: 'James Cameron', age: '67', id: '1'},
-    { name: 'James Cameron', age: '67', id: '2'},
+    { name: 'John Woo', age: '76', id: '2'},
     { name: 'Peter Jackson', age: '60', id: '3'},
     { name: 'Gore Verbinski', age: '65', id: '4'},
     { name: 'Ruso Brothers', age: '50', id: '5'}
@@ -39,7 +43,13 @@ const DirectorType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        movies: {
+            type: new GraphQLList(MovieType),
+            resolve(parrent, args){
+                return _.filter(movies, { directorId: parrent.id })
+            }
+        }
     })
 });
 
