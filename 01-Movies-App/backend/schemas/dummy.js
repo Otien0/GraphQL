c6381ff1999f865fd741b0a4b45a5cedfc687772
mourice-oduor <otienomaurice364@gmail.com>
2022-08-01 16:28:50 +0300

    dummy data

---
+++ 01-Movies-App/backend/schemas/dummy.js
@@ -0,0 +1,19 @@
+const movies = [
+    { title: 'Avatar', genre: 'Sci-fi', id: '1', directorId: '1'},
+    { title: 'Titanic', genre: 'Romance', id: '2', directorId: '1'},
+    { title: 'The Killer', genre: 'Action', id: '3', directorId: '2'},
+    { title: 'Lord Of The Rings', genre: 'Adventure', id: '4', directorId: '3'},
+    { title: 'Pirates Of The Carribean', genre: 'Adventure', id: '5', directorId: '4'},
+    { title: 'Avengers', genre: 'Sci-fi', id: '6', directorId: '5'},
+    { title: 'Hard Boiled', genre: 'Action', id: '7', directorId: '2'},
+    { title: 'Face/Off', genre: 'Action', id: '8', directorId: '2'},
+    { title: 'Gray Man', genre: 'Action', id: '9', directorId: '5'}
+]
+
+const directors = [
+    { name: 'James Cameron', age: '67', id: '1'},
+    { name: 'John Woo', age: '76', id: '2'},
+    { name: 'Peter Jackson', age: '60', id: '3'},
+    { name: 'Gore Verbinski', age: '65', id: '4'},
+    { name: 'Ruso Brothers', age: '50', id: '5'}
+]
