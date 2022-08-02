3730b16fee921cd77ef461e0d7a37b306716b600
mourice-oduor <otienomaurice364@gmail.com>
2022-08-02 19:23:22 +0300

    update component

--- 01-Movies-App/frontend/src/components/MovieList.jsx
+++ 01-Movies-App/frontend/src/components/MovieList.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client';
 import { GET_MOVIES_QUERY } from '../queries/queries';
+import MovieDetails from './MovieDetails';
+
 
 function MovieList() {
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
-  const { loading, data, error } = useQuery(GET_MOVIES_QUERY)
+  const { loading, data, error } = useQuery(GET_MOVIES_QUERY);
+
   if(loading) return <p> Loading... </p>
   
   const renderMovies = () => {
     return(
       data.movies.map(movie => {
         return(
-          <li key={movie.id}>{movie.title}</li>
+          <li key={movie.id} onClick={(e)=> setSelectedMovie(movie.id)}>{movie.title}</li>
         )
       })
     )
@@ -23,6 +27,7 @@ function MovieList() {
         <ul>
           {renderMovies()}
         </ul>
+        <MovieDetails selectedMovie={selectedMovie}/>
     </div>
   )
 }
