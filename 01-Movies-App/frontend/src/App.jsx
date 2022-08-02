6a2d0f98665e580a99f8abb3e17c52afdfaf1b09
mourice-oduor <otienomaurice364@gmail.com>
2022-08-02 15:18:52 +0300

    implement apollo-client in fetching data

--- 01-Movies-App/frontend/src/App.jsx
+++ 01-Movies-App/frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import MovieList from "./components/MovieList"
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+})
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Movie App Using MERNG</h1>
-      <MovieList/>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Movie App Using MERNG</h1>
+        <MovieList/>
     </div>
+    </ApolloProvider>
   )
 }
 
