bb42832cd3d33ee2cce406a22dd71976396a21ed
mourice-oduor <otienomaurice364@gmail.com>
2022-08-02 18:29:18 +0300

    update frontend

--- 01-Movies-App/frontend/src/App.jsx
+++ 01-Movies-App/frontend/src/App.jsx
@@ -1,24 +1,24 @@
-import MovieList from "./components/MovieList"
+import MovieList from "./components/MovieList";
 import AddMovies from "./components/AddMovies";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache()
-})
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+  name: "Movie-App",
+  version: "1.0.0",
+});
 
 function App() {
-
   return (
     <ApolloProvider client={client}>
       <div className="App">
         <h1>Movie App Using MERNG</h1>
-        <MovieList/>
-        <AddMovies/>
-    </div>
+        <MovieList />
+        <AddMovies />
+      </div>
     </ApolloProvider>
-  )
+  );
 }
 
-export default App
+export default App;
