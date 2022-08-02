a67770bc9307e4e0b295277635cf058e0c079cfd
mourice-oduor <otienomaurice364@gmail.com>
2022-08-02 19:23:42 +0300

    added a movie-Detail-Query

--- 01-Movies-App/frontend/src/queries/queries.jsx
+++ 01-Movies-App/frontend/src/queries/queries.jsx
@@ -19,6 +19,24 @@ const GET_DIRECTORS_QUERY = gql`
     }
   }
 `
+const GET_MOVIE_QUERY = gql`
+  query($id: ID){
+    movie(id: $id){
+      id
+      title
+      genre
+      director{
+        id
+        name
+        age
+        movies{
+          title
+          id
+        }
+      }
+    }
+  }
+`
 
 const ADD_MOVIE_MUTATION = gql`
     mutation( $title: String!, $genre: String!, $directorId: ID! ){
@@ -29,4 +47,4 @@ const ADD_MOVIE_MUTATION = gql`
     }
 `
 
-export { GET_MOVIES_QUERY, GET_DIRECTORS_QUERY, ADD_MOVIE_MUTATION }
+export { GET_MOVIES_QUERY, GET_DIRECTORS_QUERY, ADD_MOVIE_MUTATION, GET_MOVIE_QUERY }
