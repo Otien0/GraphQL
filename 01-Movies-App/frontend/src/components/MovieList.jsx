c2af875ff7fc1d4ff68fb5154f1e706c7e178415
mourice-oduor <otienomaurice364@gmail.com>
2022-08-02 15:19:14 +0300

    update component with apollo-client

--- 01-Movies-App/frontend/src/components/MovieList.jsx
+++ 01-Movies-App/frontend/src/components/MovieList.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client';
+
+
+const GET_MOVIES_QUERY = gql`
+  {
+    movies{
+      title
+      genre
+      id
+    }
+  }
+`
 
 function MovieList() {
+
+  const { loading, data, error } = useQuery(GET_MOVIES_QUERY)
+  if(loading) return <p> Loading... </p>
+  
+  const renderMovies = () => {
+    return(
+      data.movies.map(movie => {
+        return(
+          <li key={movie.id}>{movie.title}</li>
+        )
+      })
+    )
+  }
+
+
   return (
     <div>
         <ul>
-            <li>Movie List</li>
+          {renderMovies()}
         </ul>
     </div>
   )
