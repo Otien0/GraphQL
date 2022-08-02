c923845498c7198c788264d721c0afa00a45df78
mourice-oduor <otienomaurice364@gmail.com>
2022-08-02 18:30:01 +0300

    update component with refetchQueries

--- 01-Movies-App/frontend/src/components/AddMovies.jsx
+++ 01-Movies-App/frontend/src/components/AddMovies.jsx
@@ -1,51 +1,66 @@
-import React from 'react'
-import { useQuery } from '@apollo/client';
-import { GET_DIRECTORS_QUERY } from '../queries/queries';
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_DIRECTORS_QUERY, ADD_MOVIE_MUTATION, GET_MOVIES_QUERY } from "../queries/queries";
 
 function AddMovies() {
+  const [title, setTitle] = useState("");
+  const [genre, setGenre] = useState("");
+  const [directorID, setDirectorID] = useState("");
+  const [addMovie] = useMutation(ADD_MOVIE_MUTATION);
 
-  const { loading, data, error } = useQuery(GET_DIRECTORS_QUERY)
+
+  const { loading, data, error } = useQuery(GET_DIRECTORS_QUERY);
 
   const renderDirectors = () => {
-    if(loading) return <option disabled> Loading...</option>
-    if(error) return <option disabled> Something Went Wrong!</option>
+    if (loading) return <option disabled> Loading...</option>;
+    if (error) return <option disabled> Something Went Wrong!</option>;
 
-    return data.directors.map( director => {
-        return <option key={director.id}>{director.name}</option>
-    })
+    return data.directors.map((director) => {
+      return <option key={director.id} value={director.id}>{director.name}</option>;
+    });
+  };
 
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    addMovie({
+        variables: {
+            title,
+            genre,
+            directorId : directorID,
+        },
+        refetchQueries: [{ query: GET_MOVIES_QUERY }]
+    });
+    console.log(title, genre, directorID);
   }
 
   return (
     <div>
-        <form id='add-movie'>
+      <form id="add-movie" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="movie-title">Movie Title:</label>
+          <input id="movie-title" name="movie-title" type="text" onChange={(e)=> setTitle(e.target.value)}/>
+        </div>
 
-            <div>
-                <label htmlFor="movie-name">Movie Name:</label>
-                <input id="movie-name" name="movie-name" type="text" />
-            </div>
+        <div>
+          <label htmlFor="genre">Genre:</label>
+          <input id="genre" name="genre" type="text" onChange={(e)=> setGenre(e.target.value)} />
+        </div>
 
-            <div>
-                <label htmlFor="genre">Genre:</label>
-                <input id="genre" name="genre" type="text" />
-            </div>
+        <div>
+          <label htmlFor="director">Director:</label>
 
-            <div>
-                <label htmlFor="director">Director:</label>
+          <select name="directorId" id="director" onChange={(e)=> setDirectorID(e.target.value)}>
+            <option>Select a Director</option>
+            {renderDirectors()}
+          </select>
+        </div>
 
-                <select name="director" id="director">
-                    <option>Select a Director</option>
-                    {renderDirectors()}
-                </select>
-            </div>
-
-            <div>
-                <button type="submit"> Add New Movie</button>
-            </div>
-
-        </form>
+        <div>
+          <button type="submit"> Add New Movie</button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default AddMovies
+export default AddMovies;
